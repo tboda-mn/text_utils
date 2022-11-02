167d0f38f2cdbbdbc59492b6630293fb1d2eb7fb
Sai Thanuj Boda <sboda@modeln.com>
2022-11-02 10:46:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+let name="Thanuj"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi earum nisi placeat odit. Enim veritatis quibusdam, amet repellendus delectus impedit nobis praesentium eligendi. Fuga itaque illum quae nihil, excepturi dignissimos.</p>
     </div>
+    </>
   );
 }
 
